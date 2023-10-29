9491a3184d04ff29821ec11e23aeb102ccbbb55c
DavDeDev <pietrocoladavid@gmail.com>
2023-10-29 12:20:10 -0400

    feat: try try

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+const path = require('path')
+ 
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+ 
+module.exports = {
+  '*.(ts|tsx)': () => 'bun tsc --noEmit',
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.(md|json)': filenames => `bun prettier --write ${filenames.join(' ')}`
+}
