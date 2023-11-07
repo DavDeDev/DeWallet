8af4f6103792a44db7af68dbe12c4d57d7956add
DavDeDev <pietrocoladavid@gmail.com>
2023-11-06 23:00:50 -0500

    fix: fix favicon and lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,5 @@
 const path = require('path');
 
-const buildTscCommand = filenames =>
-  `tsc --noEmit ${filenames
-    .map(f => path.relative(process.cwd(), f))
-    .join(' ')}`;
-
 const prettierCommand = filenames => ` bun prettier -w ${filenames.join(' ')}`;
 
 const buildEslintCommand = filenames =>
@@ -13,7 +8,6 @@ const buildEslintCommand = filenames =>
     .join(' --file ')}`;
 
 module.exports = {
-  '*.(ts|tsx)': [buildTscCommand],
   '*.{js,jsx,ts,tsx}': [prettierCommand, buildEslintCommand],
   '*.(md|json)': filenames => `bun prettier --write ${filenames.join(' ')}`
 };
